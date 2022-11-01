a53e45d745b7543aa814e12157907f17c1fb76a8
JackBosco <boscoj24@mail.wlu.edu>
2022-11-01 15:49:52 -0400

    finished module

--- pyArgs.py
+++ pyArgs.py
@@ -11,14 +11,12 @@ self.args : {name : (value, optional)}"""
 class pyArgs :
     def __init__(self, args:list()):
         self.given = sys.argv[1:]
+        self.literal = args
         self.params = {}
         self.required = []
         for a in args:
             if a[-1] == '!':
-                self.params[a[:-1]] = ''
                 self.required.append(a[:-1])
-            else:
-                self.params[a] = ''
     def parse(self):
         i = 0
         while i < len(self.given) and i < 1000:
@@ -30,13 +28,15 @@ class pyArgs :
                     self.params[s[0]] = s[1]
             else:
                 #parse unnamed arg in order of args left to parse
-                for k in self.params.keys():
-                    if self.params[k] == '':
-                        self.params[k] = self.given[i]
+                for l in self.literal:
+                    if self.literal not in self.params.keys():
+                        self.params[l] = self.given[i]
             i+=1
         if i == 1000:
             raise Exception("Took too long to parse args")
         for r in self.required:
-            if self.params[r] == '':
-                raise Exception("Non-optional argument" + a[:-1] + "not given")
+            if r not in self.params.keys():
+                raise Exception("Missing required argument: " + r)
+                
+
         
